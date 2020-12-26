05f79925d11b7a7766ccd4ac19599b8484774fcd
mtbishop <bishopmatthew76@gmail.com>
2020-12-26 00:41:51 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+var inputText = $('#textInput');
+var forumID = $('#questionEat');
+var restList = $('#restaurantList');
+var restCount = $('#restaurantCount');
+
+var foodList = [];
+
+function renderFood() {
+  restList.innerHTML = '';
+  restCount = foodList.length;
+
+  for (let i = 0; i < foodList.length; i++) {
+    var foodList = foodList[i];
+
+    var foodItem = document.createElement('li');
+
+    li.textContent = foodList;
+    li.setAttribute = ('data-index', i);
+
+    var removeButton = document.createElement('button');
+    button.textContent = 'X';
+
+    li.appendChild(removeButton);
+    restList.appendChild(foodItem);
+  }
+}
+
+forumID.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var foodText = inputText.value.trim();
+
+  if (foodText === '') {
+    return;
+  }
+
+  foodList.push(foodText);
+  inputText.value = '';
+
+  renderFood();
+});
+
+restList.addEventListener('click', function (event) {
+  var element = event.target;
+
+  if (element.matches('button') === true) {
+    var index = element.parentElement.getAttribute('data=index');
+    foodList.splice(index, 1);
+
+    renderFood();
+  }
+});
